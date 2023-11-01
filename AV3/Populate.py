d3c6ed48366ef330d0f7e8ad2db4aa30704d3b6d
Gabriel Sá <102604902+GabrielSaH@users.noreply.github.com>
2023-10-31 21:36:23 -0300

    Update Populate.py

--- AV3/Populate.py
+++ AV3/Populate.py
@@ -11,6 +11,8 @@ mydb = mysql.connector.connect(
 
 cursor = mydb.cursor()
 
+cursor.execute("CREATE TABLE av3_c.Pessoa (cpf varchar(15), nome varchar(50), email varchar(100), telefone varchar(10), v INT, CONSTRAINT PK_av3 PRIMARY KEY (cpf, v)");
+
 
 for i in range(100):
     first_name = names.get_first_name()
